8cb3076e0fbe9f00d67fd93d7f3c40c69050d82a
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2023-07-24 14:51:19 +0530

    Resolved

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 #Addition , Multiplication and Subtraction of 3 numbers
 
 def addition(a,b,c):
+    print("Addition values:")
     return a + b + c
-def multiplication(a,b):
+    
+def multiplication(a,b,c):
+    print("Multiplied values:")
+
     return a * b * c
-def subtraction(a,b):
+
+def subtraction(a,b,c):
+    print("Subtraction values:")
     return a - b - c
