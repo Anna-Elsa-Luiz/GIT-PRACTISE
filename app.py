613e82f215033d35a82bd5bfc0fc52ef7548abdb
Anna Elsa Luiz <134391890+Anna-Elsa-Luiz@users.noreply.github.com>
2023-07-24 14:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-def addition(a,b):
-    return a + b 
+#Addition , Multiplication and Subtraction of 3 numbers
+
+def addition(a,b,c):
+    return a + b + c
 def multiplication(a,b):
     return a * b
-
 def subtraction(a,b):
     return a-b
